c8ac2cc3bac67aaff8e4bc6562c4dc324fdb9c00
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-07 11:43:01 +0800

    子分类

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  name: { type: String }
+  name: { type: String },
+  //分类关联
+  parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Category' },
 })
 
 module.exports = mongoose.model('Category', schema)
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -12,16 +12,16 @@ module.exports = app => {
     const model = await Category.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
-
+//分类删除
   router.delete('/categories/:id', async (req, res) => {
     await Category.findByIdAndDelete(req.params.id, req.body)
     res.send({
       success: true
     })
   })
-
+//分类列表关联字段
   router.get('/categories', async (req, res) => {
-    const items = await Category.find().limit(10)
+    const items = await Category.find().populate('parent').limit(10)
     res.send(items)
   })
 
