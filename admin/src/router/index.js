e4b9f89b417dca7c46e723abfc235025674523d1
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-07 21:23:39 +0800

    装备（物品）管理

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -5,6 +5,9 @@ import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
 
+import ItemEdit from '../views/ItemEdit.vue'
+import ItemList from '../views/ItemList.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,16 +19,13 @@ const routes = [
       {path: '/categories/create', component: CategoryEdit },
       {path: '/categories/edit/:id', component: CategoryEdit, props: true },
       {path: '/categories/list', component: CategoryList },
+
+      {path: '/items/create', component: ItemEdit },
+      {path: '/items/edit/:id', component: ItemEdit, props: true },
+      {path: '/items/list', component: ItemList },
     ]
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+
 ]
 
 const router = new VueRouter({
