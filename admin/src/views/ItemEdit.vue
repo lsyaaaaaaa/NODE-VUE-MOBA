745a467931139db3884a390a599cab892541a6ef
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-07 22:05:43 +0800

    图片上传

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -6,7 +6,15 @@
         <el-input v-model="model.name"></el-input>
       </el-form-item>
       <el-form-item label="图标">
-        <el-input v-model="model.icon"></el-input>
+        <el-upload
+          class="avatar-uploader"
+          :action="$http.defaults.baseURL + '/upload'"
+          :show-file-list="false"
+          :on-success="afterUpload"
+        >
+          <img v-if="model.icon" :src="model.icon" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -27,9 +35,12 @@ export default {
     };
   },
   methods: {
+    afterUpload(res) {
+      this.$set(this.model, 'icon', res.url)
+      this.model.icon = res.url
+    },
     async save() {
       let res; // eslint-disable-line no-unused-vars
-
       if (this.id) {
         res = await this.$http.put(`rest/items/${this.id}`, this.model);
       } else {
@@ -52,3 +63,29 @@ export default {
   },
 };
 </script>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
--- admin/src/views/ItemList.vue
+++ admin/src/views/ItemList.vue
@@ -4,6 +4,11 @@
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="240"></el-table-column>
       <el-table-column prop="name" label="物品名称"></el-table-column>
+      <el-table-column prop="icon" label="图标">
+        <template slot-scope="scope">
+          <img :src="scope.row.icon" alt="" style="height:3rem;">
+        </template>
+      </el-table-column>
       <el-table-column fixed="right" label="操作" width="180">
         <template slot-scope="scope">
           <el-button
@@ -38,15 +43,14 @@ export default {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning",
-      })
-      .then(async () => {
-        const res = await this.$http.delete(`rest/items/${row._id}`)// eslint-disable-line no-unused-vars
+      }).then(async () => {
+        const res = await this.$http.delete(`rest/items/${row._id}`); // eslint-disable-line no-unused-vars
         this.$message({
           type: "success",
           message: "删除成功!",
         });
-        this.fetch()
-      })
+        this.fetch();
+      });
     },
   },
   created() {
