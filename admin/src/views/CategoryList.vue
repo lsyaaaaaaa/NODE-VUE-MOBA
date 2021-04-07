ffeca1748081de04c5ef88ffdc227f9f188b32eb
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-07 11:14:34 +0800

    分类删除

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -6,8 +6,15 @@
       <el-table-column prop="name" label="分类名称"></el-table-column>
       <el-table-column fixed="right" label="操作" width="180">
         <template slot-scope="scope">
-          <el-button type="text" size="small" 
-          @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click="$router.push(`/categories/edit/${scope.row._id}`)"
+            >编辑</el-button
+          >
+          <el-button type="text" size="small" @click="remove(scope.row)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -25,6 +32,21 @@ export default {
     async fetch() {
       const res = await this.$http.get("categories");
       this.items = res.data;
+    },
+    async remove(row) {
+      this.$confirm(`是否确定要删除分类 "${row.name}"`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+      .then(async () => {
+        const res = await this.$http.delete(`categories/${row._id}`)// eslint-disable-line no-unused-vars
+        this.$message({
+          type: "success",
+          message: "删除成功!",
+        });
+        this.fetch()
+      })
     },
   },
   created() {
