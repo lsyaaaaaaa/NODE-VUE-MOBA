7f7e304e389ba57a930508700b8aa3098c40eaa8
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-08 11:20:28 +0800

    英雄编辑

--- admin/src/views/HeroList.vue
+++ admin/src/views/HeroList.vue
@@ -4,9 +4,10 @@
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="240"></el-table-column>
       <el-table-column prop="name" label="英雄名称"></el-table-column>
+      <el-table-column prop="title" label="称号"></el-table-column>
       <el-table-column prop="avatar" label="头像">
         <template slot-scope="scope">
-          <img :src="scope.row.avatar" alt="" style="height:3rem;">
+          <img :src="scope.row.avatar" alt="" style="height: 3rem" />
         </template>
       </el-table-column>
       <el-table-column fixed="right" label="操作" width="180">
