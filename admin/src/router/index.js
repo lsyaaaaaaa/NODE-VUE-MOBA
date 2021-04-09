6fa34d293e24ca6f9c754c8e6fc7cd1a0e0c4ff9
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-09 11:28:03 +0800

    登录界面

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
@@ -23,34 +23,35 @@ import AdminUserList from '../views/AdminUserList.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/login', name: 'login', component: Login },
   {
     path: '/',
     name: 'main',
     component: Main,
     children: [
-      {path: '/categories/create', component: CategoryEdit },
-      {path: '/categories/edit/:id', component: CategoryEdit, props: true },
-      {path: '/categories/list', component: CategoryList },
+      { path: '/categories/create', component: CategoryEdit },
+      { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+      { path: '/categories/list', component: CategoryList },
 
-      {path: '/items/create', component: ItemEdit },
-      {path: '/items/edit/:id', component: ItemEdit, props: true },
-      {path: '/items/list', component: ItemList },
+      { path: '/items/create', component: ItemEdit },
+      { path: '/items/edit/:id', component: ItemEdit, props: true },
+      { path: '/items/list', component: ItemList },
 
-      {path: '/heroes/create', component: HeroEdit },
-      {path: '/heroes/edit/:id', component: HeroEdit, props: true },
-      {path: '/heroes/list', component: HeroList },
+      { path: '/heroes/create', component: HeroEdit },
+      { path: '/heroes/edit/:id', component: HeroEdit, props: true },
+      { path: '/heroes/list', component: HeroList },
 
-      {path: '/articles/create', component: ArticleEdit },
-      {path: '/articles/edit/:id', component: ArticleEdit, props: true },
-      {path: '/articles/list', component: ArticleList },
+      { path: '/articles/create', component: ArticleEdit },
+      { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+      { path: '/articles/list', component: ArticleList },
 
-      {path: '/ads/create', component: AdEdit },
-      {path: '/ads/edit/:id', component: AdEdit, props: true },
-      {path: '/ads/list', component: AdList },
+      { path: '/ads/create', component: AdEdit },
+      { path: '/ads/edit/:id', component: AdEdit, props: true },
+      { path: '/ads/list', component: AdList },
 
-      {path: '/admin_users/create', component: AdminUserEdit },
-      {path: '/admin_users/edit/:id', component: AdminUserEdit, props: true },
-      {path: '/admin_users/list', component: AdminUserList },
+      { path: '/admin_users/create', component: AdminUserEdit },
+      { path: '/admin_users/edit/:id', component: AdminUserEdit, props: true },
+      { path: '/admin_users/list', component: AdminUserList },
     ]
   },
 
