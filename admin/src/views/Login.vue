9dd7fccac2df927187c1469da6e82248e5a0cd85
lsyaaaaaaa <lsyaaaaaaa@163.com>
2021-04-11 18:03:03 +0800

    登录接口（下）

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -28,7 +28,13 @@ export default {
   methods: {
     async login(){
       const res = await this.$http.post('login', this.model)
-      console.log(res.data)
+      //sessionStorage.token = res.data.token
+      localStorage.token = res.data.token
+      this.$router.push('/')
+      this.$message({
+        type:'success',
+        message:'登录成功'
+      })
     }
   }
 }
